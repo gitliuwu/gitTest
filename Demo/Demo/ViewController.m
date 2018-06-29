04e5eb35cffde99e49399051ea98b995b80efcf1
liuxiaowu <18765129353@163.com>
2018-06-29 15:36:41 +0800

    1

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    [[NSBundle mainBundle] pathForResource:<#(nullable NSString *)#> ofType:<#(nullable NSString *)#>]
+    //[[NSBundle mainBundle] pathForResource:<#(nullable NSString *)#> ofType:<#(nullable NSString *)#>]
+    NSLog(@"hello");
 }
 
 
